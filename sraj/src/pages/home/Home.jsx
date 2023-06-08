f378a94ed501dfcc725f9cb632a2bcdc33b5bfe0
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-06-08 17:44:41 +0530

    Made Banner using Tailwind

--- sraj/src/pages/home/Home.jsx
+++ sraj/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import Banner from '../../components/Banner/Banner';
 import 'react-toastify/dist/ReactToastify.css';
 function Home() {
     return (
         <>
-            <div>Home</div>
+            <Banner/>
         </>
     )
 }
